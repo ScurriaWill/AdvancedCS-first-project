c9e4ee9010d683624610af2bb6289e63b05895c3
21scurria <willscurria@gmail.com>
2021-02-10 10:46:19 -0500

    making combination of codes compatible

--- first project code.py
+++ first project code.py
@@ -10,28 +10,28 @@ mn = MNIST('./mnist_data')
 train_images, train_labels = mn.load_training()
 test_images, test_labels = mn.load_testing()
 
-"""x_train = np.asarray(x_train).astype(np.float32)
-y_train = np.asarray(y_train).astype(np.int32)
-x_test = np.asarray(x_test).astype(np.float32)
-y_test = np.asarray(y_test).astype(np.int32)
+train_images = np.asarray(train_images).astype(np.float32)
+train_labels = np.asarray(train_labels).astype(np.int32)
+test_images = np.asarray(test_images).astype(np.float32)
+test_labels = np.asarray(test_labels).astype(np.int32)
 
 # the data, split between train and Digit-Recognizer sets
 # (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-print(x_train.shape, y_train.shape)
+print(train_images.shape, train_labels.shape)
 
-x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
-x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
+test_images = test_images.reshape((test_images.shape[0], 28, 28, 1))
 input_shape = (28, 28, 1)
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
+train_labels = keras.utils.to_categorical(train_labels, 10)
+test_labels = keras.utils.to_categorical(test_labels, 10)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255"""
+train_images = train_images.astype('float32')
+test_images = test_images.astype('float32')
+train_images /= 255
+test_images /= 255
 
 # Reshaping data-Adding number of channels as 1 (Grayscale images)
 train_images = train_images.reshape((train_images.shape[0],
@@ -47,8 +47,8 @@ train_images = train_images.astype('float32') / 255
 test_images = test_images.astype('float32') / 255
 
 # Encoding labels to a binary class matrix
-y_train = to_categorical(train_labels)
-y_test = to_categorical(test_labels)
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
 
 print('train_images shape:', train_images.shape)
 print(train_images.shape[0], 'train samples')
@@ -84,8 +84,8 @@ model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=['ac
 
 val_images = train_images[:10000]
 partial_images = train_images[10000:]
-val_labels = y_train[:10000]
-partial_labels = y_train[10000:]
+val_labels = train_labels[:10000]
+partial_labels = train_labels[10000:]
 
 earlyStopping = callbacks.EarlyStopping(monitor="val_loss", mode="auto", patience=5, restore_best_weights=True)
 
@@ -94,7 +94,7 @@ hist = model.fit(partial_images, partial_labels, batch_size=None,
                  callbacks=[earlyStopping])
 print("The model has successfully trained")
 
-score = model.evaluate(test_images, y_test, verbose=0)
+score = model.evaluate(test_images, test_labels, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 print("CNN Error: %.2f%%" % (100 - score[1]*100))
