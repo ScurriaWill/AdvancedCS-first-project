6aeb72ec868beefe013154f73944af72ed2faf16
21scurria <willscurria@gmail.com>
2021-02-10 11:27:56 -0500

    finally fixed error in load_data!

--- first project code.py
+++ first project code.py
@@ -6,7 +6,19 @@ from keras.models import Sequential
 from keras.utils.np_utils import to_categorical
 # from mnist import MNIST
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data('mnist_data/mnist.npz')
+# requests.packages.urllib3.disable_warnings()
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 """mn = MNIST('./mnist_data')
 train_images, train_labels = mn.load_training()
